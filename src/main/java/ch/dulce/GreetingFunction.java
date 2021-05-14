05b16798cf1db9205cc372adc9345b2379066acd
Antonio Dolce <dolce.antonio@gmail.com>
2021-05-14 15:30:01 +0200

    added topic

--- src/main/java/ch/dulce/GreetingFunction.java
+++ src/main/java/ch/dulce/GreetingFunction.java
@@ -1,9 +1,20 @@
 package ch.dulce;
 
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import io.quarkus.funqy.Funq;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 public class GreetingFunction {
 
+    @Inject
+    SnsClient sns;
+    
+    @ConfigProperty(name = "topic.arn")
+    String topicArn;
+    
     @Funq
     public String emails(Email email) {
         return "hello from: " + email.getFrom();
